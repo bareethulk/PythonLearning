e7baa9a82bf5aed3350d238b59959d6a450d323f
bareethulk <bareethkader14@gmail.com>
2025-07-12 18:23:04 -0400

    Update app.py

--- 1. Primitive type/app.py
+++ 1. Primitive type/app.py
@@ -4,4 +4,12 @@ weight = float(input('enter your weight in kg: '))
 height_cm = float(input('enter your heigh in cm: '))
 height_m = height_cm / 100
 bmi = weight/(height_m ** 2)
-print(bmi)
+print(f"Your BMI is {bmi:.2f}")
+if bmi < 18.5:
+    print("You are underweight.")
+elif bmi < 25:
+    print("You have a normal weight.")
+elif bmi < 30:
+    print("You are overweight.")
+else:
+    print("You are obese.")
